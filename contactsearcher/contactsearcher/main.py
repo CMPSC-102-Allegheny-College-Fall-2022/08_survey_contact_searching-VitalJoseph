27424816693add20c769d0831fda6486b648a4a1
VitalJoseph <joseph01@allegheny.edu>
2022-11-14 23:56:57 -0500

    completed todos in main.py

--- contactsearcher/contactsearcher/main.py
+++ contactsearcher/contactsearcher/main.py
@@ -1,8 +1,13 @@
 """Define the command-line interface for the contact searching program."""
 
-# TODO: Add all of the required import statements to this module
+# Add all of the required import statements to this module
+import typer
+from pathlib import Path
+from typing import Optional
+from contactsearcher import search
 
-# TODO: create a Typer object to support the command-line interface
+# create a Typer object to support the command-line interface
+cli = typer.Typer()
 
 
 @cli.command()
@@ -33,9 +38,15 @@ def contactsearcher(
     typer.echo(
         f'  We are looking for contacts who have a job related to "{job_description}":'
     )
-    # TODO: perform the search for all of the relevant email addresses given the job description
-    # TODO: we know that there are some contacts in the list, so iterate through the list of
+    # perform the search for all of the relevant email addresses given the job description
+    relevant_emails = search.search_for_email_given_job(job_description, contacts_file)
+    # we know that there are some contacts in the list, so iterate through the list of
     # the contacts and display them in the terminal window
-    # TODO: display final information about the program's behavior in the terminal window;
+    typer.echo()
+    for emails in relevant_emails:
+        typer.echo(f"  {emails[0]} is a {emails[1]}")
+    # display final information about the program's behavior in the terminal window;
     # this should summarize whether or not the program found any matches
-    # TODO: refer to the expected output on Discord and/or Proactive Programmers for details
+    # refer to the expected output on Discord and/or Proactive Programmers for details
+    typer.echo()
+    typer.echo("Wow, we found some contacts! Email them to learn about your job!")
